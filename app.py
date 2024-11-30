c1ff80b26f27fd7ef07c66649c8829cfc747cc9e
adidabberu <adabberu@asu.edu>
2024-11-29 18:54:31 -0700

    corrected port

--- app.py
+++ app.py
@@ -50,3 +50,7 @@ def create_app():
 
 # Create the app instance
 app = create_app()
+
+# Run the application on the desired port
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
